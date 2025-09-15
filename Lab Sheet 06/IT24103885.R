5983f9fa6cf6e3b52e1a2dfb0d16e4834fa26c19
Chamal Senarathna <101007824+chamals3n4@users.noreply.github.com>
2025-09-15 19:32:24 +0530

    1757944940170

---
+++ Lab Sheet 06/IT24103885.R
@@ -0,0 +1,20 @@
+## Question 01
+
+# Part 01
+# Binomial Distribution with n=50, p=0.85
+
+# Part 02
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+## Question 02
+
+# Part 01
+#number of customer calls received in an hour
+
+# Part 02
+# poisson distribution with lambda=12
+
+
+# Part 03
+
+dpois(15, 12)
